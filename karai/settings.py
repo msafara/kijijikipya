fd4bb3769d6eb8a3a9f6b56803296c62e24450bb
lee <leengugi59@gmail.com>
2020-11-30 19:46:39 +0300

    heroku tweaked app

--- karai/settings.py
+++ karai/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
+import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -49,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',   
 ]
 
 ROOT_URLCONF = 'karai.urls'
@@ -126,9 +130,14 @@ STATICFILES_DIRS=[
 ]
 
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
 #email settings 
 EMAIL_HOST='smtp.gmail.com'
 EMAil_PORT= 587
 EMAIL_HOST_USER = 'gitukungugi@gmail.com'
 EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
+
+django_heroku.settings(locals())
